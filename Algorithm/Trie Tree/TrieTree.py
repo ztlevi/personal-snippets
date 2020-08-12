f993d09f2640503c93f7db9388a359e6c277bc97
Ting Zhou <ztlevi.work@gmail.com>
2020-08-12 14:54:01 -0700

    up

--- Algorithm/Trie Tree/TrieTree.py
+++ Algorithm/Trie Tree/TrieTree.py
@@ -9,7 +9,6 @@ class TrieNode:
 
 
 class Trie:
-
     def __init__(self):
         self.root = TrieNode()
 
@@ -34,3 +33,51 @@ class Trie:
             if current is None:
                 return False
         return True
+
+
+class Trie2:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.tree = {}
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        cur = self.tree
+        for c in word:
+            if c not in cur:
+                cur[c] = {}
+            cur = cur[c]
+        cur["#"] = "#"
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        cur = self.tree
+        for c in word:
+            if c not in cur:
+                return False
+            cur = cur[c]
+        return "#" in cur
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        cur = self.tree
+        for c in prefix:
+            if c not in cur:
+                return False
+            cur = cur[c]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
