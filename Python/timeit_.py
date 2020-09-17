ff4728942f0d8b696baa044a5387a73f0e9f3ba2
Ting Zhou <ztlevi.work@gmail.com>
2020-09-17 11:17:39 -0700

    up

--- Python/timeit_.py
+++ Python/timeit_.py
@@ -1,4 +1,46 @@
-timeit.timeit(
-    "text = 'abcdefg'; s = bytearray(text); s[1] = 'Z'; str(s)", number=1000000
-)
+import time                                                
+
+# timeit.timeit(
+#     "text = 'abcdefg'; s = bytearray(text); s[1] = 'Z'; str(s)", number=1000000
+# )
 # 1.0387420654296875
+
+# Timeit decorator
+def timeit(method):
+
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+
+        print('%r (%r, %r) %2.2f sec' % \
+              (method.__name__, args, kw, te-ts))
+        return result
+
+    return timed
+
+class Foo(object):
+
+    @timeit
+    def foo(self, a=2, b=3):
+        time.sleep(0.2)
+
+@timeit
+def f1():
+    time.sleep(1)
+    print('f1')
+
+@timeit
+def f2(a):
+    time.sleep(2)
+    print('f2',a)
+
+@timeit
+def f3(a, *args, **kw):
+    time.sleep(0.3)
+    print('f3', args, kw)
+
+f1()
+f2(42)
+f3(42, 43, foo=2)
+Foo().foo()
